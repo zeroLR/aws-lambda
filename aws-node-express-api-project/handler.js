4dc28704dac3775601ccc39b58038697f33cafca
ceyang <ceyanglab@gmail.com>
2022-08-26 11:21:29 +0800

    initial commit

---
+++ aws-node-express-api-project/handler.js
@@ -0,0 +1,32 @@
+"use strict";
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  consol.log("fsdfsdf");
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/echo", (req, res, next) => {
+  let content = req.body;
+  return res.status(200).send(content);
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+console.log("Loading function");
+
+module.exports.handler = serverless(app);
